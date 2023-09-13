0b3330e94a393a7bdcc5f8b95486ee5faf4edc5a
EliGertz94 <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>
2023-09-13 07:28:40 +0300

    updating new method for vlan and exporting from class

--- shrun.js
+++ shrun.js
@@ -1,16 +1,23 @@
 var net = require('net');
 
-var client = new net.Socket();
-client.connect(23, '10.103.0.3', function() {
+//host is the ip
+//port 23 for telnet
+//action -> "shrun" or "vlan"
+
+function showRunningConfig(level) {
+  var client = new net.Socket();
+
+
+
+client.connect(23, "10.103.0."+level, function() {
     console.log('Connected');
-    
+   
     // Send Telnet commands
     client.write('weno1\r\n'); // Replace with your Telnet password
     client.write('en\r\n'); // Enter enable mode if needed, replace with your enable password
     client.write('weno1\r\n');
     client.write('sh run\r\n'); // Send the "show run" command
-    
-    // You can send additional Telnet commands here
+ 
    
 });
 let receivedData = '';
@@ -51,3 +58,76 @@ client.on('close', function() {
     console.log('Received Data:');
     console.log(receivedData); 
 });
+
+
+}
+
+
+function configureVlan(level,computerNum,vlan) {
+
+  var client = new net.Socket();
+
+
+
+  client.connect(23, "10.103.0."+level, function() {
+      console.log('Connected');
+     
+          // Send Telnet commands
+    client.write('weno1\r\n'); // Replace with your Telnet password
+    client.write('en\r\n'); // Enter enable mode if needed, replace with your enable password
+    client.write('weno1\r\n');
+    client.write('configure terminal\r\n'); // Enter global configuration mode
+    client.write('interface GigabitEthernet0/'+computerNum+'\r\n'); // Enter the specific interface
+    client.write('switchport access vlan '+vlan+'\r\n'); // Configure the VLAN for the interface
+    client.write('end\r\n'); // Exit configuration mode
+    client.write('write memory\r\n'); // Save the configuration
+    client.write('exit\r\n'); // Exit Telnet session
+  
+
+     
+  });
+  let receivedData = '';
+  let spaceSentCount = 0; // Number of times space has been sent
+  const maxSpaceAttempts =7; // Maximum number of space attempts
+  let timeout;
+  
+  
+  client.on('data', function(data) {
+    
+      console.log('Received: ' + data);
+      receivedData += data.toString(); // Accumulate received data as a string
+      
+      // Check for the --More-- prompt and send a space to continue
+      if (receivedData.includes('--More--')) {
+          if (spaceSentCount < maxSpaceAttempts) {
+              client.write(' ');
+              spaceSentCount++;
+              // Set a timeout to send space again if needed (adjust the delay as necessary)
+              timeout = setTimeout(function() {
+                  spaceSentCount = 0; // Reset space attempts after the timeout
+              }, 2000); // 2 seconds delay
+          }
+      } else {
+          // Cancel the timeout when the prompt is no longer present
+          clearTimeout(timeout);
+      }
+      
+      // To close the connection after receiving the desired data, you can add a condition.
+      if (spaceSentCount === maxSpaceAttempts && !receivedData.includes('--More--')) {
+          client.destroy();
+      }
+  });
+  
+  client.on('close', function() {
+      
+      console.log('Connection closed');
+      console.log('Received Data:');
+      console.log(receivedData); 
+  });
+  
+}
+
+module.exports = {
+  showRunningConfig,
+  configureVlan
+};
